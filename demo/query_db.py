26e82ef40ddfad26bd7d411e38522079e993b319
Ben <18824140606@163.com>
2023-07-07 14:45:58 +0800

    add demo

---
+++ demo/query_db.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+
+    作者: 小肥巴巴
+    简书: https://www.jianshu.com/u/db796a501972
+    邮箱: imyunshi@163.com
+    github: https://github.com/xiaofeipapa/python_example
+
+    您可以任意转载, 恳请保留我作为原作者, 谢谢.
+
+
+"""
+import pymysql
+
+
+host = '192.168.1.155'
+port = 15008
+db = 'medical-waste-dev'
+user = 'root'
+password = 'f43GEGsytech'
+
+
+# ---- 用pymysql 操作数据库
+def get_connection():
+    conn = pymysql.connect(host=host, port=port, db=db, user=user, password=password)
+    return conn
+
+
+def check_it():
+
+    conn = get_connection()
+
+    # 使用 cursor() 方法创建一个 dict 格式的游标对象 cursor
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    # 使用 execute()  方法执行 SQL 查询
+    cursor.execute("select count(id) as total from mw_order")
+
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchone()
+
+    print("-- 当前数量: %d " % data['total'])
+
+    # 关闭数据库连接
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    check_it()
