2964080c5ee8625e883907347bac5bce50385426
Ben <18824140606@163.com>
2023-07-08 10:36:47 +0800

    wifi_pwd_1

--- wifiPwd/wifi_pwd_1.py
+++ wifiPwd/wifi_pwd_1.py
@@ -58,12 +58,7 @@ def wifiConnect(pwd):
         else:
             print("已有wifi连接")        
     except Exception as e:
-        print 'str(Exception):\t', str(Exception)
-        print 'str(e):\t\t', str(e)
-        print 'repr(e):\t', repr(e)
-        print 'e.message:\t', e.message
-        print 'traceback.print_exc():'; traceback.print_exc()
-        print 'traceback.format_exc():\n%s' % traceback.format_exc()
+        print(e)
     print("2")
 
 
